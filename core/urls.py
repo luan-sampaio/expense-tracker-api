eb8f7d9edeb69644b16a0f5eed5abd81b9b0bbd4
Luan Sampaio <luansampaio0294@gmail.com>
2026-04-09 10:48:57 -0300

    registrar router da API em /api/

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
-from django.http import HttpResponse
 from django.urls import path
+from ninja import NinjaAPI
 
+from transactions.api import router as transactions_router
 
-def health(request):
-    return HttpResponse("ok")
+api = NinjaAPI(title="Track Book API", version="1.0.0", docs_url="/docs")
 
+api.add_router("/transactions", transactions_router)
 
 urlpatterns = [
-    path("health/", health),
+    path("api/", api.urls),
 ]
